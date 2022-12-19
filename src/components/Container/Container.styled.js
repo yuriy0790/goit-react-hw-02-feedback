84c62552615c49240475bb85ebe98170d1e0b2c3
Yurii Oborskiy <yuriioborskiy@gmail.com>
2022-12-19 04:21:18 +0200

    styles

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   max-width: 1200px;
+  display: flex;
+  gap: 10px;
+  flex-wrap: wrap;
+  justify-content: center;
 
   padding-left: 15px;
   padding-right: 15px;
@@ -10,5 +14,5 @@ export const Container = styled.div`
   margin-left: auto;
   margin-right: auto;
 
-  background-color: rgb(240, 240, 240);
+  background-color: #d8a7ff;
 `;
